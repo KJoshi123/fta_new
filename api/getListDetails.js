3ca837ddb738a0a75f606f783b87e80337262e4e
kaustubh joshi <joshigikaustubh@gmail.com>
2021-11-22 20:10:31 +0530

    router changed to app and added dependecies

--- api/getListDetails.js
+++ api/getListDetails.js
@@ -1,8 +1,7 @@
-const router = require('express').Router();
 const fteexedte = require('../models/fteexedte');
 
 
-router.get('/', (req, res) =>{
+/*router.get('/', (req, res) =>{
     console.log('Inside getListDetails api');
 
     //get user id from request
@@ -32,6 +31,36 @@ router.get('/', (req, res) =>{
         console.log("Out of getListDetails api")
         res.json({responseData : returnObject});
     });
-});
 
-module.exports = router
+    //Trying to send sample response
+    const returnString = "sample return output";
+    res.set("Name","kaustubh")
+    console.log("Out of getListDetails API")
+    return returnString;
+});*/
+
+
+async function getListDetails(userid){
+    console.log(`Inside getListDetails api service for user id ${userid}`);
+    const responseObject = []
+    const returnObject = []
+    fteexedte.find({userid : userid}, async (error,data) => {
+        console.log(`data received \n ${data}`);
+        
+        if(error) throw error;
+        if(!data){
+            console.log(`No data found for user = ${userid}`);
+            return returnObject;
+        }
+        //return details
+        data.forEach( (item) => {
+            returnObject.push(item);
+        })
+    });
+    console.log("Out of getListDetails service api")
+    responseObject["statusCode"] = 200
+    responseObject["message"] = "success"
+    responseObject["data"] = returnObject?returnObject:[]
+    return responseObject;
+}
+exports.getListDetails = getListDetails
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 require('dotenv').config({path: __dirname + '/.env'});
 const express = require('express')
 const dbObject = require('./config/dbconnect')
+const bodyparser = require("body-parser")
+const listDetails = require("./api/getListDetails");
+
+
 const app = express()
 //Creating datbase connection
 console.log("Trying to connect db")
 dbObject(process.env.MONGODB_URL)
 
-//defining apis
-app.use('/api/getList', require('./api/getListDetails'));
+
 
 //Creating application server
 const PORT = process.env.PORT_NUMBER || 5000;
 app.listen(PORT, () => {
     console.log(`Application started at port ${PORT}`);
+})
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+
+
+
+//defining apis
+//app.use('/api/getList', require('./api/getListDetails'));
+
+app.get("/api/getList", async (req,res) => {
+    console.log("Inside getList API");
+    const userid = req.query.userid;
+    
+    const data =  await listDetails.getListDetails(userid);
+    
+    res.setHeader('Content-Type','application/json');
+    res.end(JSON.stringify(data));
 })
